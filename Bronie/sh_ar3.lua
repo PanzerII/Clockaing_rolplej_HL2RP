4c96d5fc355cd044b9e8c7e4e6baccb99c7b4795
PanzerII <60577387+PanzerII@users.noreply.github.com>
2020-02-04 21:26:25 +0100

    Dodano resztę broni palnych.

---
+++ Bronie/sh_ar3.lua
@@ -0,0 +1,12 @@
+ITEM.name = "CKM Kombinatu"
+ITEM.description = "Potężna broń, zazwyczaj stawiana na wszelkiego rodzaju barykadach. Ma ona ogromną siłę i jest w stanie przebić większość pancerzy."
+ITEM.model = "models/weapons/tfa_mmod/w_ar3.mdl"
+ITEM.class = "tfa_mmod_ar3"
+ITEM.weaponCategory = "primary"
+ITEM.width = 5
+ITEM.height = 3
+ITEM.iconCam = {
+	ang	= Angle(-0.70499622821808, 268.25439453125, 0),
+	fov	= 12.085652091515,
+	pos	= Vector(0, 200, 0)
+}
---
+++ Bronie/sh_strzelbaa.lua
@@ -0,0 +1,12 @@
+ITEM.name = "Strzelba automatyczna"
+ITEM.description = "Franchi SPAS-12, potężna broń strzelająca pociskami 20 Gauge. Niszczy wszystko co stoi na jej drodze. Ta wersja jest w pełni automatyczna."
+ITEM.model = "models/weapons/tfa_mmod/w_shotgun.mdl"
+ITEM.class = "tfa_mmod_shotgun_auto"
+ITEM.weaponCategory = "primary"
+ITEM.width = 3
+ITEM.height = 1
+ITEM.iconCam = {
+    pos = Vector(0, 200, 1),
+    ang = Angle(0, 270, 0),
+    fov = 10
+}

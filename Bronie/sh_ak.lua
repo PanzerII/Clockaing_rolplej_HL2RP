061e680450ccf9ffd8033d683c18972b7df2b66c
PanzerII <60577387+PanzerII@users.noreply.github.com>
2020-02-05 21:36:11 +0100

    Dodano karabin Kałasznikowa.

---
+++ Bronie/sh_ak.lua
@@ -0,0 +1,12 @@
+ITEM.name = "Karabin Kałasznikowa"
+ITEM.description = "Na pierwszy rzut oka wygląda jak klaszyczny 'kałach'. Nie można stwierdzić modelu przez jego zdezelowany stan."
+ITEM.model = "models/weapons/tfa_ins2/w_akm_bw.mdl"
+ITEM.class = "tfa_ins2_akm_bw"
+ITEM.weaponCategory = "primary"
+ITEM.width = 4
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.70499622821808, 268.25439453125, 0),
+	fov	= 12.085652091515,
+	pos	= Vector(0, 200, 0)
+}
